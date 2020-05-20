3756a2e024934c28a80d46bf69c5c3ea67f49736
Sayan Chatterjee <Sayan.Chatterjee@shell.com>
2020-05-20 23:48:41 +0530

    Added Ref

--- SampleAR/Model/PlaneAnimation.swift
+++ SampleAR/Model/PlaneAnimation.swift
@@ -6,6 +6,13 @@
 //  Copyright © 2020 Sayan Chatterjee. All rights reserved.
 //
 
+//MARK: - REF
+//https://www.appcoda.com/arkit-horizontal-plane/
+//https://developer.apple.com/documentation/arkit/detecting_images_in_an_ar_experience
+//https://www.appcoda.com/arkit-image-recognition/
+//https://medium.com/krootl/arkit-tutorial-image-recognition-and-virtual-content-transform-91484ceaf5d5
+
+
 import Foundation
 import SceneKit
 
